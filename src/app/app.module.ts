8152ff12b7d412f2089d7a4b5deab9fa4e5978d8
Vittorio Lo Preiato <vittorio.lo.preiato@avanade.com>
2020-03-28 17:58:55 +0100

    [app] - added elements, converted in web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -8,16 +9,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
+  entryComponents: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    {provide: APP_BASE_HREF, useValue : '/' },
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+  ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {
+  }
+  ngDoBootstrap() {
+    const component = (createCustomElement(AppComponent, { injector: this.injector }) as any);
+    customElements.define('base-feature', component);
+  }
+}
